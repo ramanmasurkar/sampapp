216e71b288081d57f55693c7ea9293b67280fd28
raman masurkar <ramanmasurkar@gmail.com>
2014-12-27 22:56:38 +0530

    signup path integration test(ex 5.6)

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,5 +11,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    
+  end
+
+   test "signup path" do
+    get signup_path
+    assert_template 'users/new'
+    assert_select "title","Sign up | Ruby on Rails Tutorial Sample App" 
   end
 end
