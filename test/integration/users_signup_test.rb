5ced79922840b5968c828f702ea800a503df0f22
raman masurkar <ramanmasurkar@gmail.com>
2014-12-28 21:43:58 +0530

    Add error msg test,flash test and content_tag for flash[aplication.html]

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,6 +13,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select "div#error_explanation"
+    assert_select "div.field_with_errors"
   end
 
   test "Valid signup information" do
@@ -21,5 +23,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     post_via_redirect users_path, user: { name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password"}
   end
    assert_template 'users/show'
+   assert_not flash.empty?
   end 
 end
