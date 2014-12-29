487a0e91c37dd3bef799c33176cdf6bffe14c9e5
raman masurkar <ramanmasurkar@gmail.com>
2014-12-29 20:01:33 +0530

    Add Remember me[session/new,css, update controller-create]

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,7 +12,7 @@ end
 
 def current_user
   if(user_id = session[:user_id])
-  @current_user ||= User.find_by(id: user_id])
+  @current_user ||= User.find_by(id: user_id)
   elsif(user_id = cookies.signed[:user_id])
   user = User.find_by(id: user_id)
   if user && user.authenticated?(cookies[:remember_token])
