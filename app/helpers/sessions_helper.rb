9de320d3707f5bb1be9412d9e7c52ce1f95d359c
raman masurkar <ramanmasurkar@gmail.com>
2014-12-30 20:06:08 +0530

    Add friendly forwarding[sessionhelper,sessioncontroller,userscontroller,users edit test]

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -38,4 +38,13 @@ end
     @current_user = nil
   end
 
+  def redirect_back_or(default)
+   redirect_to(session[:forwarding_url]||default)
+   session.delete(:forwarding_url)
+  end
+
+  def store_location
+   session[:forwarding_url] = request.url if request.get?
+  end
+
 end
