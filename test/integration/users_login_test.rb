2a8161c54a33fde285ecfff9785329a46c9399ca
raman masurkar <ramanmasurkar@gmail.com>
2014-12-30 13:06:38 +0530

    Add bug-fix for logout in second tab and second browser

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -19,7 +19,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   assert flash.empty? 
   end
 
-  test "login with valid information" do
+  test "login with valid information followed by logout" do
   get login_path
   post login_path, session: {email: @user.email, password:'password'}
   assert is_logged_in?
@@ -32,6 +32,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   delete logout_path
   assert_not is_logged_in?
   assert_redirected_to root_url
+  delete logout_path
   follow_redirect!
   assert_select "a[href=?]", login_path
   assert_select "a[href=?]", logout_path, count:0
