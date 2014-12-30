7d0593136b5f7fb4f1cee6ca43dc5df19829af46
raman masurkar <ramanmasurkar@gmail.com>
2014-12-30 12:56:29 +0530

    Add forget user(user.rb,sessionhelper)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,12 @@ def remember(user)
  cookies.permanent[:remember_token] = user.remember_token
 end
 
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
 def current_user
   if(user_id = session[:user_id])
   @current_user ||= User.find_by(id: user_id)
@@ -26,9 +32,10 @@ def logged_in?
  !current_user.nil?
 end
 
-def log_out
-  session.delete(:user_id)
-  @current_user = nil
-end
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,10 @@ class User < ActiveRecord::Base
    self.remember_token = User.new_token
    update_attribute(:remember_digest, User.digest(remember_token))
   end
+ 
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 
   def authenticated?(remember_token)
    BCrypt::Password.new(remember_digest).is_password?(remember_token)
