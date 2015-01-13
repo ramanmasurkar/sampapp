e030928f7b559442a7c7e346c4f824e3f530090a
raman masurkar <ramanmasurkar@gmail.com>
2015-01-13 14:49:29 +0530

    Accountactivation controller/ add_activation_to_users {migration}/ db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   get 'sessions/new'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
-
+  resources :account_activations, only: [:edit]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
