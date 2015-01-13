16607fc08a846300397ac8b874386c1ccd26e6e6
raman masurkar <ramanmasurkar@gmail.com>
2015-01-13 22:50:55 +0530

    update test password_reset[user_mailer_test] / Add edit[password_resets] / Add get_user,valid_user[password_resets_controller] / Add update,user_params,password_blank?,check_expiration actions[password_resets-controller] /def password_reset_expired?[user.rb]

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only:[:edit, :update]
+  before_action :valid_user, only:[:edit, :update]
+  before_action :check_expiration, only:[:edit, :update]
   def new
   end
 
@@ -6,7 +9,7 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
       @user.create_reset_digest
-      @user.send-password_reset_email
+      @user.send_password_reset_email
       flash[:info] = "Email sent with password reset instructions"
       redirect_to root_url
     else
@@ -16,4 +19,44 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  def update
+    if password_blank?
+      flash.now[:danger] = "password can't be blank"
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      flash[:success] = "password has been reset"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    def password_blank?
+      params[:user][:password].blank?
+    end
+
+    def get_user
+      @user = user.find_by(email: params[:email])
+    end
+
+    def valid_user
+      unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
+
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "password reset has expired"
+        redirect_to new_password_reset_url
+      end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -47,9 +47,13 @@ class User < ActiveRecord::Base
     update_attribute(:reset_digest, User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
   end
-  
+
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
    private
