e817c07062b5115bd1a094585d1ff7daebea6940
raman masurkar <ramanmasurkar@gmail.com>
2015-01-15 12:24:42 +0530

    modify picure_uploader.rb / add initializers/carrier_wave.rb

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,11 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for GoogleDrive
+      :provider              => 'GoogleDrive',
+      :USERNAME    => ENV['USERNAME'],
+      :PASSWORD => ENV['PASSWORD']
+    }
+    config.fog_directory     =  ENV['Images']
+  end
+end
