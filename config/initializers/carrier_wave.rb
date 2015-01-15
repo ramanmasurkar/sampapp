24dc54d70923577cef87f62c57b82833eb125f30
raman masurkar <ramanmasurkar@gmail.com>
2015-01-15 18:30:55 +0530

    production.rb temp fix

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,10 +2,10 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for GoogleDrive
-      :provider              => 'GoogleDrive',
-      :USERNAME    => ENV['USERNAME'],
-      :PASSWORD => ENV['PASSWORD']
+      #:provider    => 'GoogleDrive',
+      #:USERNAME    => ENV['USERNAME'],
+      #:PASSWORD => ENV['PASSWORD']
     }
-    config.fog_directory     =  ENV['Images']
+    #config.fog_directory     =  ENV['Images']
   end
 end
