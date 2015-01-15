17960aa98a76ce2e35f3c8bb30f8eab9fd7d390f
raman masurkar <ramanmasurkar@gmail.com>
2015-01-15 22:09:11 +0530

    add has_many :passive_relships,has_many :follwers {userr.rb} / add assert followers.include? {user_test}

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,9 @@ class User < ActiveRecord::Base
   validates :password ,length:{minimum: 6},allow_blank: true
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
