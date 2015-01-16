41eb16cf5b92774e243a159c295f75fa2b5e44a6
raman masurkar <ramanmasurkar@gmail.com>
2015-01-16 18:40:33 +0530

    add tests follow|unfollow in standard|Ajax {following_test}

--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -8,12 +8,12 @@ class RelationshipsControllerTest < ActionController::TestCase
     assert_no_difference 'Relationship.count' do
       post :create
     end
-    assert_redireted_to login_url
+    assert_redirected_to login_url
   end
 
   test "destroy should require logged-in user" do
     assert_no_difference 'Relationship.count' do
-      delete :destroy, id: relationships[:one]
+      delete :destroy, id: relationships(:one)
     end
     assert_redirected_to login_url
   end
